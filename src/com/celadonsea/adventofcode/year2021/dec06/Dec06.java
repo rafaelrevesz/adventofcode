efff99a6ce4f6724693717dc32e395df6abcb1c8
rafael.revesz <rafael.revesz@evosoft.com>
2021-12-06 07:45:31 +0100

    Fix typo

--- src/com/celadonsea/adventofcode/year2021/dec06/Dec06.java
+++ src/com/celadonsea/adventofcode/year2021/dec06/Dec06.java
@@ -11,14 +11,14 @@ public class Dec06 {
 
     private static final int NUMBER_OF_DAYS = 256;
 
-    private static final Map<Integer, BigDecimal> laternFishAges = new HashMap<>();
+    private static final Map<Integer, BigDecimal> lanternFishAges = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         String line;
         try (BufferedReader br = new BufferedReader(new FileReader("src/com/celadonsea/adventofcode/year2021/dec06/dec06.txt"))) {
             line = br.readLine();
         }
-        laterfishLife(line.split(","));
+        lanternFishLife(line.split(","));
         // task 1
         //   sample: 5934
         //   puzzle: 359344
@@ -27,25 +27,25 @@ public class Dec06 {
         //  puzzle: 1629570219571
     }
 
-    private static void laterfishLife(String[] ages) {
+    private static void lanternFishLife(String[] ages) {
         for (int age = 0; age < 9; age++) {
-            laternFishAges.put(age, new BigDecimal(0));
+            lanternFishAges.put(age, new BigDecimal(0));
         }
         for (String age : ages) {
             int ageKey = Integer.parseInt(age);
-            BigDecimal currentCount = laternFishAges.get(ageKey);
-            laternFishAges.put(ageKey, currentCount.add(new BigDecimal(1)));
+            BigDecimal currentCount = lanternFishAges.get(ageKey);
+            lanternFishAges.put(ageKey, currentCount.add(new BigDecimal(1)));
         }
         for (int i = 0; i < NUMBER_OF_DAYS; i++) {
-            BigDecimal countOfAge0 = laternFishAges.get(0);
+            BigDecimal countOfAge0 = lanternFishAges.get(0);
             for (int age = 0 ; age < 8; age++) {
-                laternFishAges.put(age, laternFishAges.get(age + 1));
+                lanternFishAges.put(age, lanternFishAges.get(age + 1));
             }
-            laternFishAges.put(8, countOfAge0);
-            laternFishAges.put(6, laternFishAges.get(6).add(countOfAge0));
+            lanternFishAges.put(8, countOfAge0);
+            lanternFishAges.put(6, lanternFishAges.get(6).add(countOfAge0));
         }
         BigDecimal sum = new BigDecimal(0);
-        for (BigDecimal ageCounter : laternFishAges.values()) {
+        for (BigDecimal ageCounter : lanternFishAges.values()) {
             sum = sum.add(ageCounter);
         }
         System.out.println("Result: " + sum.toString());
