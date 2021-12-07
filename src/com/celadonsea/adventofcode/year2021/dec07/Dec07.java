d5fbdc039e3c538de52a110652f5b2f8f209b1e4
rafael.revesz <rafael.revesz@evosoft.com>
2021-12-07 08:24:44 +0100

    December 7

--- src/com/celadonsea/adventofcode/year2021/dec07/Dec07.java
+++ src/com/celadonsea/adventofcode/year2021/dec07/Dec07.java
@@ -3,6 +3,7 @@ package com.celadonsea.adventofcode.year2021.dec07;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,29 +27,20 @@ public class Dec07 {
     private static void crabsMove(List<Integer> crabsPosition) {
         int min = crabsPosition.stream().mapToInt(i -> i).min().getAsInt();
         int max = crabsPosition.stream().mapToInt(i -> i).max().getAsInt();
-        long minDistance = Long.MAX_VALUE;
-        long minFuel = Long.MAX_VALUE;
-        int minDistancePosition = 0;
-        int minFuelPosition = 0;
-        for (int target = min; target <= max; target++) {
-            long distance = 0;
-            long fuel = 0;
-            for (int pos : crabsPosition) {
-                distance += Math.abs(target - pos);
-                fuel += gauss(Math.abs(target - pos));
-            }
-            if (minDistance >= distance) {
-                minDistance = distance;
-                minDistancePosition = target;
-            }
-            if (minFuel >= fuel) {
-                minFuel = fuel;
-                minFuelPosition = target;
-            }
-            minFuel = Math.min(minFuel, fuel);
-        }
-        System.out.println("Min distance (" + minDistancePosition + "): " + minDistance);
-        System.out.println("Min fuel (" + minFuelPosition + "): " + minFuel);
+        List<Integer> list = new ArrayList<>();
+        for (int i = min; i <= max; i++) list.add(i);
+        int minDistance = list
+                .stream()
+                .map(target -> crabsPosition.stream().map(p -> Math.abs(target - p)).reduce(0, Integer::sum))
+                .min(Integer::compareTo)
+                .get();
+        long minFuel = list
+                .stream()
+                .map(target -> crabsPosition.stream().map(p -> Math.abs(target - p)).map(Dec07::gauss).reduce(0L, Long::sum))
+                .min(Long::compareTo)
+                .get();
+        System.out.println("Min distance: " + minDistance);
+        System.out.println("Min fuel: " + minFuel);
     }
 
     private static long gauss(long n) {
