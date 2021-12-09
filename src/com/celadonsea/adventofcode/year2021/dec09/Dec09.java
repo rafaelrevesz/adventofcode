5d81ea6b9d142a4f89633efea23b99322460f442
rafael.revesz <rafael.revesz@evosoft.com>
2021-12-09 21:30:53 +0100

    December 9 - without optimization

--- src/com/celadonsea/adventofcode/year2021/dec09/Dec09.java
+++ src/com/celadonsea/adventofcode/year2021/dec09/Dec09.java
@@ -4,12 +4,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Dec09 {
 
+    private static List<LowPoint> lowPoints = new ArrayList<>();
+    private static List<Basin> basins = new ArrayList<>();
+    private static List<String> lines = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
-        List<String> lines = new ArrayList<>();
         int result = 0;
         try (BufferedReader br = new BufferedReader(new FileReader("src/com/celadonsea/adventofcode/year2021/dec09/dec09.txt"))) {
             String line;
@@ -21,14 +27,62 @@ public class Dec09 {
         for (int i = 0; i < lines.size(); i++) {
             String up = i == 0 ? null : lines.get(i - 1);
             String down = i == lines.size() - 1 ? null : lines.get(i + 1);
-            result += findLowPoints(up, lines.get(i), down);
+            result += findLowPoints(up, lines.get(i), down, i);
+        }
+        for (LowPoint lowPoint : lowPoints) {
+            Basin basin = new Basin(lowPoint);
+            basins.add(basin);
+            mapABasin(lowPoint, basin);
         }
         System.out.println("Result: " + result);
-        // 15
+        Collections.sort(basins, (lhs, rhs) -> {
+            // -1 - less than, 1 - greater than, 0 - equal, all inversed for descending
+            return Integer.compare(rhs.points.size(), lhs.points.size());
+        });
+        int basinResult = basins.get(0).points.size() *
+                basins.get(1).points.size() *
+                basins.get(2).points.size();
+        System.out.println("Basin result: " + basinResult);
         // 600
+        // 987840
     }
 
-    private static int findLowPoints(String test1, String test2, String test3) {
+    private static void mapABasin(LowPoint lowPoint, Basin basin) {
+        Integer up = getBasinPoint(lowPoint.x, lowPoint.y - 1);
+        Integer down = getBasinPoint(lowPoint.x, lowPoint.y + 1);
+        Integer left = getBasinPoint(lowPoint.x - 1, lowPoint.y);
+        Integer right = getBasinPoint(lowPoint.x + 1, lowPoint.y);
+        if (up != null && up > lowPoint.number && !basin.pointExists(lowPoint.x, lowPoint.y - 1)) {
+            LowPoint point = new LowPoint(lowPoint.x, lowPoint.y - 1, up);
+            basin.points.add(point);
+            mapABasin(point, basin);
+        }
+        if (down != null && down > lowPoint.number && !basin.pointExists(lowPoint.x, lowPoint.y + 1)) {
+            LowPoint point = new LowPoint(lowPoint.x, lowPoint.y + 1, down);
+            basin.points.add(point);
+            mapABasin(point, basin);
+        }
+        if (left != null && left > lowPoint.number && !basin.pointExists(lowPoint.x - 1, lowPoint.y)) {
+            LowPoint point = new LowPoint(lowPoint.x - 1, lowPoint.y, left);
+            basin.points.add(point);
+            mapABasin(point, basin);
+        }
+        if (right != null && right > lowPoint.number && !basin.pointExists(lowPoint.x + 1, lowPoint.y)) {
+            LowPoint point = new LowPoint(lowPoint.x + 1, lowPoint.y, right);
+            basin.points.add(point);
+            mapABasin(point, basin);
+        }
+    }
+
+    private static Integer getBasinPoint(int x, int y) {
+        if (x < lines.get(0).length() && x >= 0 && y < lines.size() && y >= 0) {
+            int i = Integer.parseInt("" + lines.get(y).charAt(x));
+            return i < 9 ? i : null;
+        }
+        return null;
+    }
+
+    private static int findLowPoints(String test1, String test2, String test3, int row) {
         int result = 0;
         if (test2 != null && (test1 != null || test3 != null)) {
             for (int i = 0; i < test2.length(); i++) {
@@ -37,11 +91,47 @@ public class Dec09 {
                 boolean up = test1 == null || test1.charAt(i) > test2.charAt(i);
                 boolean down = test3 == null || test3.charAt(i) > test2.charAt(i);
                 if (left && right && up && down) {
-                    result += Integer.parseInt("" + test2.charAt(i)) + 1;
+                    int lowPointValue = Integer.parseInt("" + test2.charAt(i));
+                    result += lowPointValue + 1;
+                    lowPoints.add(new LowPoint(i, row, lowPointValue));
                 }
             }
         }
         return result;
     }
 
+    private static class Basin {
+        Set<LowPoint> points = new HashSet<>();
+
+        Basin(LowPoint startingPoint) {
+            points.add(startingPoint);
+        }
+
+        boolean pointExists(int x, int y) {
+            boolean exists = false;
+            for (LowPoint point : points) {
+                exists |= (point.x == x && point.y == y);
+            }
+            return exists;
+        }
+    }
+
+    private static class LowPoint {
+        int x;
+        int y;
+        int number;
+        LowPoint(int x, int y, int number) {
+            this.x = x;
+            this.y = y;
+            this.number = number;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            LowPoint lowPoint = (LowPoint) o;
+            return x == lowPoint.x && y == lowPoint.y;
+        }
+    }
 }
